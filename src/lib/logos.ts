d0fbd7b56746f0cb58b1b151f65c86a0b7eb1640
jechamo <109816631+jechamo@users.noreply.github.com>
2025-12-02 08:34:33 +0100

    Create PARTY_LOGOS and TEAM_LOGOS dictionaries
    Added a centralized dictionary of party and team logos.

---
+++ src/lib/logos.ts
@@ -0,0 +1,38 @@
+// Diccionario centralizado de logotipos
+export const PARTY_LOGOS: Record<string, string> = {
+  "PSOE": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Logotipo_del_PSOE.svg/267px-Logotipo_del_PSOE.svg.png",
+  "Partido Popular": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Partido_Popular.svg/177px-Partido_Popular.svg.png",
+  "VOX": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/VOX_logo.svg/365px-VOX_logo.svg.png",
+  "Sumar": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Sumar_logo.svg/512px-Sumar_logo.svg.png",
+  "ERC": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/ERC_logotipo_compacto.svg/50px-ERC_logotipo_compacto.svg.png",
+  "Junts": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Logotip_Junts_per_Catalunya.svg/100px-Logotip_Junts_per_Catalunya.svg.png",
+  "Bildu": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Icono_de_EH_Bildu_%282023%29.svg/282px-Icono_de_EH_Bildu_%282023%29.svg.png",
+  "PNV": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Icono_EAJ-PNV_2025.svg/605px-Icono_EAJ-PNV_2025.svg.png",
+  "Podemos": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Podemos_logo_2020.svg/279px-Podemos_logo_2020.svg.png",
+  "BNG": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/BNG_logo.svg/400px-BNG_logo.svg.png",
+  "Compromís": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Comprom%C3%ADs-Podemos-%C3%89s_el_moment.svg/344px-Comprom%C3%ADs-Podemos-%C3%89s_el_moment.svg.png",
+  "Ciudadanos": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Ciudadanos_logo_2017.svg/580px-Ciudadanos_logo_2017.svg.png",
+};
+
+export const TEAM_LOGOS: Record<string, string> = {
+  "FC Barcelona": "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/512px-FC_Barcelona_%28crest%29.svg.png",
+  "Real Madrid": "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/512px-Real_Madrid_CF.svg.png",
+  "Atlético de Madrid": "https://upload.wikimedia.org/wikinews/en/thumb/c/c1/Atletico_Madrid_logo.svg/274px-Atletico_Madrid_logo.svg.png",
+  "Athletic Club": "https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Club_Athletic_Bilbao_logo.svg/512px-Club_Athletic_Bilbao_logo.svg.png",
+  "Real Betis": "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Real_betis_logo.svg/512px-Real_betis_logo.svg.png",
+  "Sevilla FC": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Sevilla_FC_logo.svg/512px-Sevilla_FC_logo.svg.png",
+  "Real Sociedad": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Real_Sociedad_logo.svg/512px-Real_Sociedad_logo.svg.png",
+  "Villarreal CF": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Escudo_de_Vila-Real.svg/960px-Escudo_de_Vila-Real.svg.png",
+  "Valencia CF": "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Valenciacf.svg/512px-Valenciacf.svg.png",
+  "RCD Espanyol": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Rcd_espanyol_logo.svg/512px-Rcd_espanyol_logo.svg.png",
+  "Getafe CF": "https://upload.wikimedia.org/wikipedia/commons/b/b9/Getafe_CF_Logo.png",
+  "RCD Mallorca": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Rcd_mallorca.svg/362px-Rcd_mallorca.svg.png",
+  "CA Osasuna": "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Osasuna_logo.svg/512px-Osasuna_logo.svg.png",
+  "Rayo Vallecano": "https://upload.wikimedia.org/wikipedia/an/c/cc/Rayo_Vallecano_logo.svg.png",
+  "Celta de Vigo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/RC_Celta_de_Vigo_logo.svg/512px-RC_Celta_de_Vigo_logo.svg.png",
+  "Deportivo Alavés": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Deportivo_Alaves_logo_%282020%29.svg/300px-Deportivo_Alaves_logo_%282020%29.svg.png",
+  "Girona FC": "https://upload.wikimedia.org/wikipedia/ca/thumb/a/a5/For_article_Girona_FC.svg.png/600px-For_article_Girona_FC.svg.png",
+  "UD Las Palmas": "https://upload.wikimedia.org/wikipedia/sco/thumb/2/20/UD_Las_Palmas_logo.svg/152px-UD_Las_Palmas_logo.svg.png",
+  "CD Leganés": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Escudo_de_Legan%C3%A9s_%28Madrid%29.svg/500px-Escudo_de_Legan%C3%A9s_%28Madrid%29.svg.png",
+  "Real Valladolid": "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Real_Valladolid_Logo.svg/512px-Real_Valladolid_Logo.svg.png"
+};
