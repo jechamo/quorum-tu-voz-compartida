dfbd8d819eb2d4b63ec541f80a98a5b7ec5e5a68
Jechamo <jchamorrorodriguez@gmail.com>
2025-12-06 22:39:30 +0100

    Update package dependencies and remove unused assets
    - Added @capacitor/android, @capacitor/assets, and @capacitor/cli to dependencies.
    - Removed the assets/readme.md file as it is no longer needed.

---
+++ android/app/src/main/java/com/tuvozcompartida/quorum/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tuvozcompartida.quorum;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
+
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuvozcompartida.quorum',
+  appName: 'quorum',
+  webDir: 'dist'
+};
+
+export default config;
